26963c7fcd538ec35d11f5ec75ac9d32d43f3c47
JailtoGN <jailtongn@outlook.com>
2025-07-18 16:55:11 -0300

    Sincroniza arquivos modificados e adiciona novos arquivos ao projeto

---
+++ src/__init__.py
@@ -0,0 +1,14 @@
+"""
+MarketWar - Pacote Principal
+============================
+
+Este pacote contém todos os módulos do MarketWar:
+- scrapers: Scrapers dos supermercados
+- models: Modelos de dados
+- api: APIs REST
+- utils: Utilitários
+"""
+
+__version__ = "1.0.0"
+__author__ = "JailtonGN"
+__description__ = "Comparador de Preços de Supermercados - Natal/RN" 
